91ac22b9079299ecaf3c07a9fc0f92df8420858a
GarwachiHoussem <garwachi.houssem@gmail.com>
2023-06-30 23:04:37 +0200

    Button-clicker

---
+++ Week2/Day 1/Core/Button_clicker/script.js
@@ -0,0 +1,10 @@
+function Logout(element) {
+    console.log(element.innerText='Logout');
+}
+function remove(element) {
+    console.log(element.remove());
+}
+function custom() {
+   console.log(alert);
+    
+}
