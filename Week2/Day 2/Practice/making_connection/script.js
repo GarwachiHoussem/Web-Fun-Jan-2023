25a4a736ebad46987c41eea9a942a0fa719c9613
GarwachiHoussem <garwachi.houssem@gmail.com>
2023-07-02 02:00:59 +0200

    making_connection

---
+++ Week2/Day 2/Practice/making_connection/script.js
@@ -0,0 +1,15 @@
+
+
+
+//****remove****
+function supprimer(element) {
+    console.log(element.remove());
+}
+
+function supprimer(element) {
+    console.log(element.remove());
+}
+
+function houssem(e) {
+   e.innerText='Todd E';
+}
